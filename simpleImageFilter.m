668a23b2a28bfa7dc09d9571e6dcc29928c825ed
Harry Sarson <hds28@cam.ac.uk>
2018-05-10 10:33:12 +0100

    comparision of column first and row first

--- simpleImageFilter.m
+++ simpleImageFilter.m
@@ -1,13 +1,34 @@
 %% 5. Simple Image Filtering
 
+%% Low pass
+
 D = 256;
 N = 15;
 
 h = halfcos(N);
 
-Xf = conv2se(1, h, X);
+filteredColsFirst = conv2se(h', h, X);
+filteredRowsFirst = conv2se(h, h', X')';
 
-draw(Xf);
+diff = abs(filteredColsFirst - filteredRowsFirst);
+
+draw(diff ./ max(diff(:)) * 256);
+
+fprintf('Max absolute difference = %g\n', max(abs(diff(:))));
+fprintf('Average difference = %g\n', mean(diff(:)));
+fprintf('Variance of differences = %g = %g^2\n', ...
+    var(diff(:)), sqrt(var(diff(:))));
+
+
+
+
+
+
+
+
+
+
+
 
 
 

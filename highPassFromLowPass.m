3b417754fc248df97c83e8e319911dcfb1bf2d9f
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-05-10 14:33:08 +0100

    print grid of lp/hp images

--- highPassFromLowPass.m
+++ highPassFromLowPass.m
@@ -8,39 +8,40 @@ plots = 5;
 
 % Odd length of half-cosine filter
 for i = 1:(plots-1)
-    N = floor(5 ^ (i-1)), 255;
+    N = floor(5 ^ (i-1));
 
     h = halfcos(N);
 
     LP = conv2se(h', h, X);
     HP = X - LP;
 
-    subplot(2, plots, i);
-    image(LP);
-    title (sprintf('Filter length %g', N))
-    axis image
-    axis off;
-    subplot(2, plots, i + plots);
-    image(HP);
-    axis image
-    axis off;
+    subplot(1, plots, i);
+    plotColumn(HP, LP, sprintf('Filter length %g', N))
 end
 
-N = floor(5 ^ (i-1)), 255;
-
 % Keep DC only
-
 
 LP = ones(size(X)) * mean(X(:));
 HP = X - LP;
 
-subplot(2, plots, plots);
-image(LP);
-title (sprintf('Filter length %g', N))
-axis image
-axis off;
-subplot(2, plots, 2*plots);
-image(HP);
-axis image
-axis off;
+subplot(1, plots, plots);
+plotColumn(HP, LP, 'Filter DC only')
+
+%% plot column
+
+function plotColumn (HP, LP, columnTitle)
+    dimensions = size(LP);
+    whitespace = ones(60, dimensions(2)) * 255;
+    data = [ LP; whitespace; HP + 128; whitespace; whitespace];
+    image(data);
+    text(4, dimensions(1) + 16, sprintf('Energy %g', sum(LP(:).^2)));
+    text(4, 2*dimensions(1) + 60 + 16, sprintf('Energy %g', sum(HP(:).^2)));
+    text(4, 2*dimensions(1) + 60 + 16 + 30, sprintf('Total %g', sum(LP(:).^2 + HP(:).^2)));
+    title(columnTitle);
+    axis image;
+    axis off;
+    xlabel(sprintf('Energy %g', sum(LP(:).^2)));
+    xlabel(sprintf('Energy %g', sum(HP(:).^2)));
+end
+
 
